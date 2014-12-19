602f2cfa2a25a644251208a480cb9ce154135875
KEvin Wang <kevinwang@kevinwang.us>
2014-12-19 12:59:44 -0800

    hackerrank simples

---
+++ hackerrank/general/palindrome-index.py
@@ -0,0 +1,22 @@
+def palindex(string):
+    for i in range(len(string)//2):
+        if string[i] != string[-1*(i+1)]:
+            if string[i+1] == string[-1*(i+1)]:
+                return i
+                
+            else:
+                return (len(string) - (i+1))
+    return -1
+
+
+def main():
+    #inp = sys.stdin.read()
+    inp = """3
+aaab
+baa
+aaa"""
+    inplist = []
+    for line in inp.split('\n')[1:]:
+        print(palindex(line))
+
+main()
