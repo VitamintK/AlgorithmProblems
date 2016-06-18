927cfb25002f0a6ae2d2aa097f25a33c233a874b
Kevin Wang <kevinwang@kevinwang.us>
2016-06-18 12:03:17 -0700

    cleanup and commenting cf marathon code

--- codeforces/marathon_1/confidence.py
+++ codeforces/marathon_1/confidence.py
@@ -13,6 +13,7 @@ def random_str():
 def get_score(guess_string, true_string = None):
     assert not (true_string is None and LOCALTESTING)
     if LOCALTESTING:
+        #compare guess_string with true_string
         score = 0
         wrongs = 0
         for a, b in zip(true_string, guess_string):
@@ -22,10 +23,12 @@ def get_score(guess_string, true_string = None):
                 return max(0,score-4000) + 4000
         return 1000
     else:
+        #get the score from the online judge
         print(guess_string)
         sys.stdout.flush()
         return input()
 def try_hard(confidences, spawn):
+    #make the optimum string according to our confidences
     s = list(spawn)
     for index, ans in enumerate(s):
         if confidences[index] > 0:
@@ -39,20 +42,17 @@ def main():
         true_string = random_str()
     OG = random_str()
     confidences = [0]*N
-    score = get_score(OG, true_string) 
+    score = get_score(OG, true_string)
     spawn = OG
     for i in range(X-1):
-        #spawn = ''.join([str(1-int(x) if random.random()<CHANCE else x) for x in OG])
-        #score = get_score(guess string, true_string)
         spawn = list(try_hard(confidences, list(spawn)))
-        # bitchanges = random.sample(range(score), CHANGES)
-        bitchanges = range(i*CHANGES, i*CHANGES + CHANGES)
+        # bitchanges = random.sample(range(score), CHANGES) #randomly select bits to flip
+        bitchanges = range(i*CHANGES, i*CHANGES + CHANGES) #select the immediately proceeding bits to flip
         for bit_index in bitchanges:
             spawn[bit_index] = str(1-int(spawn[bit_index]))
         new_score = get_score(''.join(spawn), true_string)
-        confidence = (new_score - score)/2 #NOT A TRUE CALCULATION
+        confidence = (new_score - score)/2 #NOT A TRUE CALCULATION but maybe it is if i am good at math maybe
         #print("confidence: {},	old_score: {},	new_score:{}".format(confidence, score, new_score))
-        #because the change in amount correct is not simply new_score - score
         for bit_index in bitchanges:
             confidences[bit_index]+= ((int(spawn[bit_index])*2)-1)*confidence
         score = new_score
