f13fa0be6959467e3fdb6da480030fd86370e9be
KEvin Wang <kevinwang@kevinwang.us>
2017-05-11 13:55:04 -0700

    fixed CF round #413 problem c code

--- codeforces/round413/c.cpp
+++ codeforces/round413/c.cpp
@@ -48,7 +48,6 @@ int main(){
 		if(D[i].first <= d)
 			best_d = max(best_d, D[i].second);
 	}
-
 	ll best_c = 0;
 	for(ll i = 0; i < C.size(); i++){
 		if(C[i].first <= c)
@@ -62,45 +61,47 @@ int main(){
 	ll cmax = 0;
 	ll cpoint = 0;
 	if(C.size() > 0){
-	for(ll i = C.size() -1; i >= 0; i--){
-		while(C[cpoint].first <= c-C[i].first && cpoint < i){
-			//cmax = max(cmax, C[cpoint].second);
-			//ans = max(ans, cmax + C[i].second);
-			cpoint++;
-		}
-		//cout << C[i].second << " " << cs[cpoint]  << endl;
-
-		if(cs[cpoint] != 0){
+		for(ll i = C.size() -1; i >= 0; i--){
 			if(cpoint > i){
-				cpoint = i;
+					cpoint = i;
+				}
+			while(C[cpoint].first <= c-C[i].first && cpoint < i){
+				//cmax = max(cmax, C[cpoint].second);
+				//ans = max(ans, cmax + C[i].second);
+				cpoint++;
 			}
-			ans = max(ans, C[i].second + cs[cpoint]);
+			//cout << C[i].second << " " << cs[cpoint]  << endl;
+
+			if(cs[cpoint] != 0){
+				
+				ans = max(ans, C[i].second + cs[cpoint]);
+			}
+			//if(cpoint == i)
+			//	break;
 		}
-		//if(cpoint == i)
-		//	break;
 	}
-}
 
 	ll dmax = 0;
 	ll dpoint = 0;
 	if(D.size() > 0){
-	for(ll i = D.size() -1; i >= 0; i--){
-		while(D[dpoint].first <= d-D[i].first && dpoint != i){
-			//cmax = max(cmax, C[cpoint].second);
-			//cout << C[i].second << " " << cmax << endl;
-			//ans = max(ans, cmax + C[i].second);
-			dpoint++;
-		}
-		if(ds[dpoint] != 0){
+		for(ll i = D.size() -1; i >= 0; i--){
 			if(dpoint > i){
-				dpoint = i;
+					dpoint = i;
+				}
+			while(D[dpoint].first <= d-D[i].first && dpoint != i){
+				//cmax = max(cmax, C[cpoint].second);
+				//cout << C[i].second << " " << cmax << endl;
+				//ans = max(ans, cmax + C[i].second);
+				dpoint++;
 			}
-			ans = max(ans, D[i].second + ds[dpoint]);
+			if(ds[dpoint] != 0){
+				
+				ans = max(ans, D[i].second + ds[dpoint]);
+			}
+			//if(dpoint == i)
+		//		break;
 		}
-		//if(dpoint == i)
-	//		break;
 	}
-}
 	cout << ans << endl;
 	return 0;
 }
