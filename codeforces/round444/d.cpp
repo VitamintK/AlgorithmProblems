4c9081547ec015278ab615ec96c066b7f5cdd9b3
KEvin Wang <kevinwang@kevinwang.us>
2017-11-22 23:15:46 -0800

    clarified unfinished status for codeforces 444 div2D

--- codeforces/round444/d.cpp
+++ codeforces/round444/d.cpp
@@ -1,3 +1,4 @@
+//unfinished.  problem statement was wrong.  i read the wrong problem statement wrong anyways.
 #include <bits/stdc++.h>
 #include <string>
 
@@ -23,36 +24,7 @@ int main(){
 	vector<ll> window(2);
 	int fpoint = -1;
 	for(int i = 0; i < n; i++){
-		if(i > 0){
-			window[xs[i-1]]--;
-		}
-		//if(fpoint < i){
-			//window[xs[i]]++;
-			//fpoint = i;
-		//}
-		ll earliest;
-			if(i == 0){
-					earliest = 0;
-				} else {
-					earliest = ts[i-1]+1;
-			}
-		while(fpoint < n-1 && ts[fpoint+1] < ts[i] + len){
-			fpoint++;
-			window[xs[fpoint]]++;
-					
-
-			if(og + (c-a)*window[1] - (d-b)*window[0] >= 0){
-				if(ts[fpoint] - earliest + 1 >= len){
-					cout << ts[fpoint] - len + 1  << endl;
-					return 0;
-				}
-			}
-		}
-		if(og + (c-a)*window[1] - (d-b)*window[0] >= 0){
-				cout << earliest << endl;
-				cout << "THIS " << endl;
-				return 0;
-		}
+		
 	}
 	if(og >= 0){
 		cout << ts[n-1] + 1 << endl;
