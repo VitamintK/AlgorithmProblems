81f07016af70ec5ec830f729897b220313fb017c
Kevin Wang <kvn.a.wng@gmail.com>
2017-11-30 03:23:53 -0800

    updated old code from hackerrank week of code 28
    for some reason i had a nonfinal (non-input-reading) version committed.

--- hackerrank/week_of_code_28/B_xor.py
+++ hackerrank/week_of_code_28/B_xor.py
@@ -1,9 +1,9 @@
 q = int(input().strip())
-q = 100
-for x in range(1,101):
+for a0 in range(q):
+    x = int(input().strip())
     b = bin(x)[2:]
     ans = 0
     for i in range(len(b)):
         if b[i] == '0':
             ans+= pow(2,len(b) - i -1)
-    print(x, ans)
+    print(ans)
