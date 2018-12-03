4ef3e03fbd5bdac1c979d7144cb1330af1644f2d
Kevin Wang <wang@shift.com>
2018-12-03 00:49:29 -0800

    changes some tabs to spaces

--- miscellaneous/advent-of-code/2018/day_2.py
+++ miscellaneous/advent-of-code/2018/day_2.py
@@ -1,38 +1,38 @@
 if False:
-	a = 0
-	b = 0
+    a = 0
+    b = 0
 
-	from collections import Counter
+    from collections import Counter
 
-	while True:
-		try:
-			s = input()
-			c = Counter(s)
-			cnts = set(c.values())
-			if 2 in cnts:
-				a += 1
-			if 3 in cnts:
-				b += 1
-		except:
-			break
+    while True:
+        try:
+            s = input()
+            c = Counter(s)
+            cnts = set(c.values())
+            if 2 in cnts:
+                a += 1
+            if 3 in cnts:
+                b += 1
+        except:
+            break
 
-	print(a*b)
+    print(a*b)
 else:
-	xs = []
-	while True:
-		try:
-			p = input()
-			for x in xs:
-				cnt = 0
-				ans = ""
-				for k in range(len(x)):
-					if x[k] == p[k]:
-						ans+=x[k]
-					else:
-						cnt += 1
-				if cnt == 1:
-					print(ans)
-					raise ValueError
-			xs.append(p)
-		except:
-			break
+    xs = []
+    while True:
+        try:
+            p = input()
+            for x in xs:
+                cnt = 0
+                ans = ""
+                for k in range(len(x)):
+                    if x[k] == p[k]:
+                        ans+=x[k]
+                    else:
+                        cnt += 1
+                if cnt == 1:
+                    print(ans)
+                    raise ValueError
+            xs.append(p)
+        except:
+            break
