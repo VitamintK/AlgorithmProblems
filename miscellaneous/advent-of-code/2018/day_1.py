397d5e78243595a0b4acf2c76b211b2b59b12b11
Kevin Wang <wang@shift.com>
2018-12-03 00:49:29 -0800

    changes some tabs to spaces

--- miscellaneous/advent-of-code/2018/day_1.py
+++ miscellaneous/advent-of-code/2018/day_1.py
@@ -1,12 +1,12 @@
 #part 1
 if False:
-	ans = 0
-	while True:
-		try:
-			ans += int(input())
-		except:
-			break
-	print(ans)
+    ans = 0
+    while True:
+        try:
+            ans += int(input())
+        except:
+            break
+    print(ans)
 
 #part 2
 
@@ -14,18 +14,18 @@ seen = set([0])
 ans = 0
 ls = []
 while True:
-	try:
-		ls.append(int(input()))
-	except:
-		break
+    try:
+        ls.append(int(input()))
+    except:
+        break
 
 for i in range(1000):
-	for j in ls:
-		ans += j
-		if ans in seen:
-			print(ans)
-			break
-		seen.add(ans)
-	else:
-		continue
-	break
+    for j in ls:
+        ans += j
+        if ans in seen:
+            print(ans)
+            break
+        seen.add(ans)
+    else:
+        continue
+    break
