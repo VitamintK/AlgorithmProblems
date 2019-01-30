7126e67f47c608e94491f2082743e5209ce6b8d6
Kevin Wang <wang@shift.com>
2019-01-30 00:18:21 -0800

    actually read the problem correctly and fixed solution

--- acm-practice/2019-01-29/B_code_cleanups.py
+++ acm-practice/2019-01-29/B_code_cleanups.py
@@ -1,21 +1,19 @@
-#didn't work
-
 thresh = 20
 days = int(input())
 xs = [int(x) for x in input().split()]
-
+xs.reverse()
 n = 0
 s = 0
 ans = 0
-for x in xs:
-    #print("adding {}".format(x))
-    n += x
+for day in range(1,366):
+    if len(xs) > 0 and xs[-1] == day:
+        n += 1
+        xs.pop()
     s += n
-    #print(n, s)
     if s >= thresh:
-        ans += 1
         n = 0
         s = 0
-if n >0 or s>0 and days == 365:
-    ans+=1
+        ans += 1
+if n > 0 or s > 0:
+    ans += 1
 print(ans)
