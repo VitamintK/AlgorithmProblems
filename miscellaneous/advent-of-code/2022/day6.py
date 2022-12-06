c74da9d479eb9b0375954a0cbd156de10e572887
Kevin Wang <kevinwang@kevinwang.us>
2022-12-06 00:10:59 -0500

    advent of code day 6

---
+++ miscellaneous/advent-of-code/2022/day6.py
@@ -0,0 +1,59 @@
+from collections import defaultdict, deque, Counter
+# d = deque()
+# d.append(5)
+# x = d.popleft()
+import re
+# m = re.match(r"(\w+) (\w+)", "Isaac Newton, physicist") 
+# # or re.search
+# >>> m.group(0)       # The entire match
+# 'Isaac Newton'
+# >>> m.group(1)       # The first parenthesized subgroup.
+# 'Isaac'
+# >>> m.group(2)       # The second parenthesized subgroup.
+# 'Newton'
+# >>> m.group(1, 2)    # Multiple arguments give us a tuple.
+# ('Isaac', 'Newton')
+from heapq import heappush, heappop
+# >>> heap = []
+# >>> data = [1, 3, 5, 7, 9, 2, 4, 6, 8, 0]
+# >>> for item in data:
+# ...     heappush(heap, item)
+# heap[0] is the smallest item
+import string
+# string.ascii_lowercase == 'abcde...'
+# string.ascii_uppercase == 'ABCDE...'
+from functools import lru_cache
+# @lru_cache(maxsize=None)
+
+import sys
+
+sys.setrecursionlimit(100000)
+
+def get_ints(s):
+    return list(map(int, re.findall(r"-?\d+", s)))  # copied from mcpower from mserrano on betaveros' recommendation
+dirs = [(0,1), (1,0), (0,-1), (-1,0)]
+octs = [(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1)]
+def is_grid_valid(n,m, r,c,):
+    return (0<=r<n) and (0<=c<m)
+
+if True:
+    ans = 0
+    inps = []
+    # while True:
+    #     try:
+    #         inps.append(input())
+    #     except EOFError:
+    #         break
+    inp = input()
+    buff = []
+    for i in range(len(inp)):
+        buff.append(inp[i])
+        if len(buff) > 14:
+            buff = buff[1:]
+        if len(set(buff)) == 14:
+            print(i+1)
+            break
+
+    print(ans)
+else:
+    pass
