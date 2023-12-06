15594be4514118bcab2aaee86edcdb8ee06e4eca
Kevin Wang <kevinwang@kevinwang.us>
2023-12-06 00:28:14 -0500

    AOC day 6 (2023)

---
+++ miscellaneous/advent-of-code/2023/day6.py
@@ -0,0 +1,65 @@
+from collections import defaultdict, deque, Counter
+# d = deque()
+# d.append(5)
+# x = d.popleft()
+import re
+# m = re.match(r"(\w+) (\w+)", "Isaac Newton, physicist") 
+# # or re.search
+# >>> m.group(0)       # The entire match
+# 'Isaac Newton'
+# >>> m.group(1)       # The first parenthesized subgroup.
+# 'Isaac'
+# >>> m.group(2)       # The second parenthesized subgroup.
+# 'Newton'
+# >>> m.group(1, 2)    # Multiple arguments give us a tuple.
+# ('Isaac', 'Newton')
+from heapq import heappush, heappop
+# >>> heap = []
+# >>> data = [1, 3, 5, 7, 9, 2, 4, 6, 8, 0]
+# >>> for item in data:
+# ...     heappush(heap, item)
+# heap[0] is the smallest item
+import string
+# string.ascii_lowercase == 'abcde...'
+# string.ascii_uppercase == 'ABCDE...'
+from functools import lru_cache
+# @lru_cache(maxsize=None)
+
+import sys
+
+sys.setrecursionlimit(100000)
+
+def get_ints(s):
+    return list(map(int, re.findall(r"-?\d+", s)))  # copied from mcpower from mserrano on betaveros' recommendation
+dirs = [(0,1), (1,0), (0,-1), (-1,0)]
+directions = 'RDLU'
+octs = [(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1)]
+def is_grid_valid(n,m, r,c,):
+    return (0<=r<n) and (0<=c<m)
+def sign_of(x):
+    if x==0:
+        return 0
+    return x/abs(x)
+
+if True:
+    ans = 1
+    times = map(int, input().split()[1:])
+    distances = map(int,(input().split()[1:]))
+    # times = [int(input().replace(' ', '').split(':')[1])]
+    # distances = [int(input().replace(' ', '').split(':')[1])]
+
+    # I used wolfram alpha for the second part. but I could have just used this script, since the puzzle input is actually just 10^7, which takes something like 1 or 10 seconds.
+    # I just assumed that it wouldn't work.
+
+    for t,d in zip(times, distances):
+        a = 0
+        for i in range(t):
+            b = (t-i) * i
+            if b > d:
+                a += 1
+        print(a)
+        ans *= a
+
+    print(ans)
+else:
+    pass
