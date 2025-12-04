9e38a1d60ce6fd77463efaf1ff39c2a1dfde8df9
Kevin Wang <kevinwang@kevinwang.us>
2025-12-04 13:29:10 -0500

    2025 aoc day 4

---
+++ miscellaneous/advent-of-code/2025/day4.py
@@ -0,0 +1,116 @@
+from collections import defaultdict, deque, Counter
+# d = deque()
+# d.append(5)
+# x = d.popleft()
+import re
+# m = re.match(r"(\w+) (\w+)", "Isaac Newton, physicist") 
+# # or re.search
+# >>> m.group(0)       # The entire match
+# 'Isaac Newton'
+# >>> m.group(1)       # The first parenthesized subgroup.
+# 'Isaac'
+# >>> m.group(2)       # The second parenthesized subgroup.
+# 'Newton'
+# >>> m.group(1, 2)    # Multiple arguments give us a tuple.
+# ('Isaac', 'Newton')
+from heapq import heappush, heappop
+# >>> heap = []
+# >>> data = [1, 3, 5, 7, 9, 2, 4, 6, 8, 0]
+# >>> for item in data:
+# ...     heappush(heap, item)
+# heap[0] is the smallest item
+import string
+# string.ascii_lowercase == 'abcde...'
+# string.ascii_uppercase == 'ABCDE...'
+from functools import lru_cache
+# @lru_cache(maxsize=None)
+import numpy as np
+
+import sys
+
+sys.setrecursionlimit(100000)
+
+def get_ints(s):
+    return list(map(int, re.findall(r"-?\d+", s)))  # copied from mcpower from mserrano on betaveros' recommendation
+dirs = [(0,1), (1,0), (0,-1), (-1,0)]
+directions = 'RDLU'
+octs = [(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1)]
+def is_grid_valid(n,m, r,c,):
+    return (0<=r<n) and (0<=c<m)
+def sign_of(x):
+    if x==0:
+        return 0
+    return x/abs(x)
+
+INTERVAL_TYPE_INCLUSIVE = 0
+INTERVAL_TYPE_EXCLUSIVE = 1
+# def make_interval_class(start_type=INTERVAL_TYPE_INCLUSIVE, end_type=INTERVAL_TYPE_EXCLUSIVE):
+#     class Interval:
+#         start_type = start_type
+#         end_type = end_type
+#         def __init__(self, start, end):
+#             self.start = start
+#             self.end = end
+# def merge(interval_a, interval_b):
+#     interval = (min(interval_a[0], interval_b[0]), max(interval_a[1], interval_b[1]))
+#     if interval[0] > interval[1]:
+#         return None
+####################################
+
+PART = 1
+PART = 2
+if PART == 1:
+    ans = 0
+    inps = []
+    while True:
+        try:
+            inps.append(input())
+        except EOFError:
+            break
+    R, C = len(inps), len(inps[0])
+    for r in range(R):
+        for c in range(C):
+            if inps[r][c] == '@':
+                rolls = 0
+                for dr, dc in octs:
+                    nr, nc = r+dr, c+dc
+                    if not is_grid_valid(R, C, nr, nc):
+                        continue
+                    if inps[nr][nc] == '@':
+                        rolls += 1
+                if rolls < 4:
+                    ans += 1
+
+    
+
+    print(ans)
+else:
+    ans = 0
+    inps = []
+    while True:
+        try:
+            inps.append([x for x in input()])
+        except EOFError:
+            break
+    R, C = len(inps), len(inps[0])
+    while True:
+        last_ans = ans
+        for r in range(R):
+            for c in range(C):
+                if inps[r][c] == '@':
+                    rolls = 0
+                    for dr, dc in octs:
+                        nr, nc = r+dr, c+dc
+                        if not is_grid_valid(R, C, nr, nc):
+                            continue
+                        if inps[nr][nc] == '@':
+                            rolls += 1
+                    if rolls < 4:
+                        ans += 1
+                        inps[r][c] = '.'
+        if ans == last_ans:
+            break
+
+    
+
+    print(ans)
